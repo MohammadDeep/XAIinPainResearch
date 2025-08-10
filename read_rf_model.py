16a9a1998e0fa07ce98bc3e4583ab35a9f58ced8
Mohammad <mmfd25285@gmail.com>
2025-08-10 15:51:17 +0330

    add read models

--- read_rf_model.py
+++ read_rf_model.py
@@ -319,8 +319,8 @@ if __name__ == "__main__":
 	
         for class_model in list_calsses:
             
-            list_dir_model = find_dir_model(class_model, f'sudject_{subject}')
-            print(f'list model in :{list_dir_model}, file : sudject_{subject}')
+            list_dir_model = find_dir_model(class_model, f'subject_{subject}')
+            print(f'list model in :{list_dir_model}, file : subject_{subject}')
 			
             for dir_model in list_dir_model:
                 print(f'dir model : {dir_model}')
