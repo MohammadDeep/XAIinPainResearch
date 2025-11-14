20a60a3c023a905f5f3eafd4e4ee37318bc13065
Mohammad <mmfd25285@gmail.com>
2025-11-14 11:01:30 +0330

    number tree = 50

--- main.py
+++ main.py
@@ -286,7 +286,7 @@ if __name__ == "__main__":
 			 [[0],[1], [2],[3], [4]],
 
 		]:
-			for n_estimators in [50, 100 , 150 ,200]:
+			for n_estimators in [50]:
 				param["classes"] = classes
 				param["n_estimators"] = n_estimators
 				param["selected_sensors"] = selected_sensors
