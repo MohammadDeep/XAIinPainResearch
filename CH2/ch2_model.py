8af9ab86a173de8a01c32d15e36b62c4fb00d39e
Mohammad <mmfd25285@gmail.com>
2025-11-14 12:58:47 +0330

    read .npy

--- CH2/ch2_model.py
+++ CH2/ch2_model.py
@@ -1,13 +1,22 @@
 import numpy as np
 
 # مسیر فایل npy
-path = input('endter dir x.npy : ')
+path = './datasets/painmonit/np-dataset/'
+x = 'X.npy'
+subjects = 'subjects.npy'
+y_covas = 'y_covas.npy'
+y_heater = 'y_heater.npy'
+def read_File(path):
+    # لود کردن
+    arr = np.load(path)
 
-# لود کردن
-arr = np.load(path)
+    print("type:", type(arr))
+    print("shape:", arr.shape)
+    print("dtype:", arr.dtype)
 
-print("type:", type(arr))
-print("shape:", arr.shape)
-print("dtype:", arr.dtype)
+for i in [x, subjects, y_covas, y_heater]:
+    print('==================================')
+    print( i )
+    read_File(path + i)
 
 
